df23643db588b67183671bc2577871ee8e233438
Mike Bond <mbond@adobe.com>
2025-05-28 14:35:41 -0700

    WIP - Add dominant direction to irradiance

--- src/textureTools/textureTools.ts
+++ src/textureTools/textureTools.ts
@@ -83,8 +83,11 @@ export class TextureTools {
      */
     public renderDiffuseIBL(texture: BaseTexture): void {
         this._cdfGenerator.onGeneratedObservable.addOnce(() => {
-            this._iblDiffuseEffect.render(texture, this._cdfGenerator.getIcdfTexture());
-            this._blitCubeEffect.blit(this._iblDiffuseEffect.rtw, 0);
+            this._cdfGenerator.findDominantDirection().then((direction) => {
+                this._iblDiffuseEffect.render(texture, this._cdfGenerator.getIcdfTexture());
+                this._blitCubeEffect.blit(this._iblDiffuseEffect.rtw, 0);
+                // TODO - Do something with the dominant direction
+            });
         });
         this._cdfGenerator.iblSource = texture;
         this._cdfGenerator.renderWhenReady();
